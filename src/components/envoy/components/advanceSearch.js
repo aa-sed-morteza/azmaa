56a4b4238f614a083b5855c6009bfb4214cab12c
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-12-06 12:07:28 +0330

    Fix advanceSearch in envoy page.

--- src/components/envoy/components/advanceSearch.js
+++ src/components/envoy/components/advanceSearch.js
@@ -3,20 +3,124 @@ import styled from "styled-components";
 import icon from "../../../assets/down.webp";
 import arrow from "../../../assets/arrowUp.webp";
 
+export default function AdvanceSearch() {
+  const [open, setOpen] = useState(false);
+  const [commission, setCommission] = useState(false);
+  const [commissionText,setCommissionText]=useState('همۀ کمیسیون‌ها')
+  const [area, setArea] = useState(false);
+  const [areaText,setAreaText]=useState('همۀ حوزه‌ها')
+  const [sex, setSex] = useState(false);
+  const [sexText,setSexText]=useState('مرد و زن')
+
+  const handleOpen = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+
+  const checkCommission = (e)=>{
+    setCommissionText(e.target.innerHTML);
+    setCommission(false);
+  }
+
+  const checkArea = (e)=>{
+    setAreaText(e.target.innerHTML);
+    setArea(false);
+  }
+
+  const checkSex =(e)=>{
+    setSexText(e.target.innerHTML);
+    setSex(false);
+  }
+  
+
+  return (
+    <Container open={open}>
+      <SearchButton onClick={handleOpen}>
+        <p className="label">{!open ? " جستجوی پیشرفته " : "جستجوی ساده"}</p>
+      </SearchButton>
+
+      {open ? (
+        <>
+          <Items style={{ marginTop: "10px" }}>
+            <DropDown
+              onClick={() => {
+                setCommission(!commission);
+              }}
+            >
+              <p className="label">{commissionText}</p>
+            </DropDown>
+            {commission && (
+              <>
+                <Item onClick={checkCommission}>ورزش</Item>
+                <Item onClick={checkCommission}>سلامت وبهداشت</Item>
+                <Item onClick={checkCommission}>امنیت ملی</Item>
+              </>
+            )}
+          </Items>
+          <Items>
+            <DropDown
+              onClick={() => {
+                setArea(!area);
+              }}
+            >
+              <p className="label">{areaText} </p>
+            </DropDown>
+            {area && (
+              <>
+                <Item onClick={checkArea}>غرب</Item>
+                <Item onClick={checkArea}>شرق</Item>
+                <Item onClick={checkArea}>شمال</Item>
+              </>
+            )}
+          </Items>
+          <Items>
+            <DropDown
+              onClick={() => {
+                setSex(!sex);
+              }}
+            >
+              <p className="label">{sexText} </p>
+            </DropDown>
+            {sex && (
+              <>
+                <Item onClick={checkSex}>مرد</Item>
+                <Item onClick={checkSex}>زن</Item>
+              </>
+            )}
+          </Items>
+        </>
+      ) : (
+        ""
+      )}
+    </Container>
+  );
+}
+
 const Container = styled.section`
   border: 1px solid #cbcbcb;
   border-radius: 4px;
   padding: 10px;
   margin-top: 10px;
-  position: relative;
+
+  @media (min-width: 480px) {
+    width: 50.5%;
+    margin: auto;
+    margin-top: 25px;
+    padding: 14px;
+    border-radius: 8px;
+  }
+`;
+
+const SearchButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.521vw;
   &:after {
     content: "";
     width: 9px;
     height: 5px;
-    left: 111px;
-    top: 19px;
-    display: block;
-    position: absolute;
+    display: inline-flex;
     background-image: url(${(props) => (props.open ? arrow : icon)});
     background-size: cover;
     background-repeat: no-repeat;
@@ -27,18 +131,10 @@ const Container = styled.section`
     margin: 0;
     color: #9f9f9f;
   }
-
   @media (min-width: 480px) {
-    width: 50.5%;
-    margin: auto;
-    margin-top: 25px;
-    padding: 14px;
-    border-radius: 8px;
     &:after {
       width: 15px;
       height: 9px;
-      left: 345px;
-      top: 27px;
     }
 
     .label {
@@ -48,25 +144,23 @@ const Container = styled.section`
 `;
 
 const DropDown = styled.div`
-  border: 1px solid #707070;
-  border-radius: 2px;
-  padding: 5px 15px;
-  margin-bottom: 10px;
-  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   &:after {
     content: "";
     width: 9px;
     height: 5px;
-    left: 15px;
-    top: 13px;
-    display: block;
-    position: absolute;
+    display: inline-flex;
     background-image: url(${icon});
     background-size: cover;
     background-repeat: no-repeat;
+    margin-right: auto;
   }
   .label {
     color: #707070;
+    margin: 0;
+    margin-right: auto;
   }
   @media (min-width: 480px) {
     padding: 15px;
@@ -74,39 +168,21 @@ const DropDown = styled.div`
     &:after {
       width: 15px;
       height: 9px;
-      left: 23px;
-      top: 26px;
     }
   }
 `;
 
-export default function AdvanceSearch() {
-  const [open, setOpen] = useState(false);
+const Items = styled.div`
+  border: 1px solid #707070;
+  border-radius: 2px;
+  padding: 5px 15px;
+  margin-bottom: 10px;
+`;
 
-  const handleOpen = (e) => {
-    e.preventDefault();
-
-    setOpen(!open);
-  };
-
-  return (
-    <Container onClick={handleOpen} open={open}>
-      <p className="label">{!open ? " جستجوی پیشرفته " : "جستجوی ساده"}</p>
-      {open ? (
-        <>
-          <DropDown style={{ marginTop: "10px" }}>
-            <p className="label">همۀ کمیسیون‌ها</p>
-          </DropDown>
-          <DropDown>
-            <p className="label">همۀ حوزه‌ها</p>
-          </DropDown>
-          <DropDown>
-            <p className="label">مرد و زن</p>
-          </DropDown>
-        </>
-      ) : (
-        ""
-      )}
-    </Container>
-  );
-}
+const Item = styled.div`
+  border: 1px solid #707070;
+  border-radius: 2px;
+  padding: 5px 15px;
+  margin-bottom: 10px;
+  text-align: center;
+`;
