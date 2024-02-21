85b710f787ad1a7bb97f45fb0e82809995e80668
mohamadreza <mohamadrezahanji@gmail.com>
2024-02-21 11:47:53 +0330

    delete console.log

--- src/components/dashboard/superviser/components/sectionRoom/components/imageSection.js
+++ src/components/dashboard/superviser/components/sectionRoom/components/imageSection.js
@@ -21,7 +21,6 @@ export default function ImageSection() {
 
   const onSubmit = async (values, actions) => {
     // dispatch({ type: "SET_IMG_ARTICLE", payload: values });
-    console.log(values);
     dispathRedux(setimgArticle(values));
     // dispatch({ type: "SET_ADD_ARTICLE", payload: 4 });
     dispathRedux(setArticleLevel(4));
